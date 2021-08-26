b6794f342f0188843301e803c988063050e63aa7
John Tucker <jtucker@spotify.com>
2021-08-26 13:21:27 -0400

    provider fix

--- main.tf
+++ main.tf
@@ -1,8 +1,30 @@
 terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "= 1.11.3"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "= 2.4.1"
+    }
+  }
   required_version = "= 1.0.5"
 }
 
 # PROVIDER
+
+provider "kubectl" {
+  alias          = "cluster-1"
+  config_path    = "~/.kube/config"
+  config_context = var.kubernetes["cluster_1"].config_context
+}
+
+provider "kubectl" {
+  alias          = "cluster-2"
+  config_path    = "~/.kube/config"
+  config_context = var.kubernetes["cluster_2"].config_context
+}
 
 provider "kubernetes" {
   alias          = "cluster_1"
@@ -25,6 +47,7 @@ module "cluster_1" {
   github_repository_name    = var.github_repository_name
   kubernetes_cluster_name   = var.kubernetes["cluster_1"].cluster_name
   providers = {
+    kubectl    = kubectl.cluster_1
     kubernetes = kubernetes.cluster_1
   }
 }
@@ -36,6 +59,7 @@ module "cluster_2" {
   github_repository_name    = var.github_repository_name
   kubernetes_cluster_name   = var.kubernetes["cluster_2"].cluster_name
   providers = {
+    kubectl    = kubectl.cluster_2
     kubernetes = kubernetes.cluster_2
   }
 }
